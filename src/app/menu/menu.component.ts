aec0ca254e9645d3f054711de87fa6e0dd0329e3
volodymyrz <volodymyr@expandi.io>
2024-03-02 13:17:27 +0200

    feature: fixed routing bug with page reloading (added routerLink), styled menu items

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+
+  constructor(private readonly router: Router) {
+  }
+
+  public isSelected(route: string): boolean {
+    return this.router.url.includes(route);
+  }
 
 }
